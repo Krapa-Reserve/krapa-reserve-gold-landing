086fdd680a5aaa9e55521d92abea0e02b25728d4
geeloko <88936372+geeloko@users.noreply.github.com>
2026-08-07 00:34:36 +0000

    feat: Add language selector and logo link in Nav
    Make Nav a client component and add a language Select control. Wrap the logo Image in a link to '/' and style the FAQ link. Import Select-related components, add a small items array, and render a SelectTrigger/SelectContent with SelectItems (defaulting to English). Minor styling adjustments for hover and font weight.

--- components/menu/nav.tsx
+++ components/menu/nav.tsx
@@ -1,5 +1,20 @@
+"use client"
+
 import Image from "next/image";
 import { buttonVariants } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const items = [
+  { label: "English", value: "english" },
+  { label: "French", value: "french" }
+]
 
 
 export default function Nav() {
@@ -9,23 +24,48 @@ export default function Nav() {
         {/** left side*/}
         <div className="flex items-center">
           {/** logo*/}
-          <Image 
-            src="/logo.png"
-            alt="Krapa Reserve Gold Logo"
-            width={0}
-            height={0}
-            sizes="180px"
-            className="w-auto h-auto"
-            loading="eager"
-          />
+          <a href="/">
+            <Image 
+              src="/logo.png"
+              alt="Krapa Reserve Gold Logo"
+              width={0}
+              height={0}
+              sizes="180px"
+              className="w-auto h-auto"
+              loading="eager"
+            />
+          </a>
         </div>
         
         {/** right side*/}
         <div className="flex items-center gap-14">
           {/** menu items*/}
           <div className="flex items-center gap-8">
-            <a href="/faq">FAQ's</a>
-            <a href="/">English</a>
+            <a 
+              href="/faq"
+              className="hover:text-primary font-bold"
+            >
+              FAQ's
+            </a>
+            <Select 
+              items={items}
+              defaultValue="english"
+            >
+              <SelectTrigger className="w-full max-w-48 hover:text-primary font-bold text-base">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent
+                alignItemWithTrigger={false}
+              >
+                <SelectGroup>
+                  {items.map((item) => (
+                    <SelectItem key={item.value} value={item.value}>
+                      {item.label}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
           {/** login/signup*/}
           <div className="flex items-center gap-2">
